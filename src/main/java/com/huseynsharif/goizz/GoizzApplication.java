cf1474b5e48419b27147beb5faf0ac1c9fa02456
Huseyn Sharifzade <huseynsharif@gmail.com>
2023-10-21 10:05:11 +0400

    First commit

---
+++ src/main/java/com/huseynsharif/goizz/GoizzApplication.java
@@ -0,0 +1,13 @@
+package com.huseynsharif.goizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoizzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoizzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huseynsharif/goizz/GoizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huseynsharif.goizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoizzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
