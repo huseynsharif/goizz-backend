54691e9785809ce1a9e092e2e1aca7f1d15a47fb
Huseyn Sharifzade <huseynsharif@gmail.com>
2023-10-22 22:57:08 +0400

    Firebase configuration

--- src/main/java/com/huseynsharif/goizz/GoizzApplication.java
+++ src/main/java/com/huseynsharif/goizz/GoizzApplication.java
@@ -1,12 +1,33 @@
 package com.huseynsharif.goizz;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class GoizzApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader = GoizzApplication.class.getClassLoader();
+
+
+        // TODO: serviceAccountKey faylini tapmir
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
         SpringApplication.run(GoizzApplication.class, args);
     }
 
